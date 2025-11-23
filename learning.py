eb10ad27fe4de692abb706a01b819b3e7d18b6c7
VibhuDevs <technicalgamer420@gmail.com>
2025-11-23 22:52:36 +0530

    clean: keep only slot.py, remove all other files

--- learning.py
+++
@@ -1,9 +0,0 @@
-def strip(l, word):
-    for item in l:
-        n = []
-        for item in l:
-            if not(item==word):
-                n.append(item.strip(word))
-    return n
-l = ["me", "word", "vibhu", "hu"]
-print(strip(l, "hu"))
